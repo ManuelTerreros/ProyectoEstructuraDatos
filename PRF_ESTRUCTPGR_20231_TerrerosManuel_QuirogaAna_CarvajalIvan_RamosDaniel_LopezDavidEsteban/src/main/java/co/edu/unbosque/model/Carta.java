e03fe63eba11025d1f9fe95da787914627b8c6ca
carol <carol@172.21.36.194>
2023-05-10 12:34:41 -0500

    Commit david

---
+++ PRF_ESTRUCTPGR_20231_TerrerosManuel_QuirogaAna_CarvajalIvan_RamosDaniel_LopezDavidEsteban/src/main/java/co/edu/unbosque/model/Carta.java
@@ -0,0 +1,28 @@
+package co.edu.unbosque.model;
+
+public class Carta {
+	private int numero;
+	private String color;
+	
+	public Carta(int numero, String color) {
+		this.numero = numero;
+		this.color = color;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+}
--- PRF_ESTRUCTPGR_20231_TerrerosManuel_QuirogaAna_CarvajalIvan_RamosDaniel_LopezDavidEsteban/src/main/java/co/edu/unbosque/model/Pila.java
+++ PRF_ESTRUCTPGR_20231_TerrerosManuel_QuirogaAna_CarvajalIvan_RamosDaniel_LopezDavidEsteban/src/main/java/co/edu/unbosque/model/Pila.java
@@ -1,5 +1,21 @@
 package co.edu.unbosque.model;
 
-public class Pila {
+import java.util.Stack;
+
+public class Pila extends Thread{
+	
+	private Stack<Carta> montoCartas;
+	
+	public Pila() {
+		montoCartas = new Stack<Carta>();
+	}
+	
+	public void run() {
+		
+	}
+	
+	public void agregarElemento(Carta elemento) {
+		montoCartas.push(elemento);
+	}
 
 }
