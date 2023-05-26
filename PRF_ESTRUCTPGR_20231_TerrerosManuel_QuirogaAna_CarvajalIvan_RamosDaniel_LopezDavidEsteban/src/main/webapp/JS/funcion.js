be6f1b8efb3a45911caad8789ea55fb6e24a6c84
ManuelTerreros <114838362+ManuelTerreros@users.noreply.github.com>
2023-05-25 20:44:09 -0500

    Delete PRF_ESTRUCTPGR_20231_TerrerosManuel_QuirogaAna_CarvajalIvan_RamosDaniel_LopezDavidEsteban directory

--- PRF_ESTRUCTPGR_20231_TerrerosManuel_QuirogaAna_CarvajalIvan_RamosDaniel_LopezDavidEsteban/src/main/webapp/JS/funcion.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", function() {
-  const loader = document.getElementById("loader");
-  setTimeout(function() {
-    loader.style.display = "none";
-  }, 2000); // tiempo de espera en milisegundos
-});
-
